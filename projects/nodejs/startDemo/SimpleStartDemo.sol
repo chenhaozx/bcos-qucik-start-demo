e843ed313e9d9ac775b26f3e7200cb049eef27c0
crisiticmei(梅芬) <crisiticmei@webank.com>
2017-08-20 22:43:59 +0800

    添加event 可以直接查看日志

--- projects/nodejs/startDemo/SimpleStartDemo.sol
+++ projects/nodejs/startDemo/SimpleStartDemo.sol
@@ -2,16 +2,18 @@ pragma solidity ^0.4.2;
 
 contract SimpleStartDemo {
     int256 storedData;
-
+    event AddMsg(address indexed sender, bytes32 msg);
     function SimpleStartDemo() {
         storedData = 2;
     }
 
     function set(int256 x) public{
         storedData = x;
+        AddMsg(msg.sender, "in the set");
     }
 
     function get() constant public returns (int256 _ret) {
-      return _ret = storedData;
+        AddMsg(msg.sender, "in the get");
+        return _ret = storedData;
     }
 }
